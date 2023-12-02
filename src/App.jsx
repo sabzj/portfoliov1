c6afdd5734d7af6ff14e73cf212de2252ab08234
sabzj <zjsabbah@gmail.com>
2023-12-02 21:48:53 +0200

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Intro from "./components/intro/Intro";
 // import "./App.css";
 // import Portfolio from "./components/portfolio/Portfolio";
 // import Testimonials from "./components/testimonials/Testimonials";
-import Topbar from "./components/topbar/Topbar";
+// import Topbar from "./components/topbar/Topbar";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Topbar />
+      {/* <Topbar /> */}
       <Intro />
       {/* <Portfolio /> */}
       <Contact />
